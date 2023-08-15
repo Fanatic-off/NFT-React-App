2e84b8443d9019565e0087cf69eb61ee4be6e02e
Eugene <kulinich.eugene.2412@gmail.com>
2023-08-16 01:57:34 +0600

    correct header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,25 +5,24 @@ import logo from '../../assets/logo.png'
 import user from '../../assets/User.svg'
 import burgerMenu from '../../assets/burgerMenu.svg'
 
-
-export const Header = () =>{
-    return(
+export const Header = () => {
+    return (
         <div>
-        <header>
-            <img src={logo} ></img>
-            <div className="layout-item">
-                <a href="#">Marketplace</a>
-                <a href="#">Rankings</a>
-                <a href="#">Connect a wallet</a>
-                <button>
-                    <img src={user}></img>
-                    Sign Up
+            <header>
+                <img src={logo} ></img>
+                <div className="layout-item">
+                    <a href="#">Marketplace</a>
+                    <a href="#">Rankings</a>
+                    <a href="#">Connect a wallet</a>
+                    <button>
+                        <img src={user}></img>
+                        Sign Up
+                    </button>
+                </div>
+                <button className="fill mobile-only">
+                    <img src={burgerMenu}></img>
                 </button>
-            </div>
-            <button className="fill mobile-only">
-                <img src={burgerMenu}></img>
-            </button>
-        </header>
+            </header>
         </div>
     );
 };
